f8014130cdb2a940fa9a117581215763b17ad5a1
Britisha26 <tisha.anants@gmail.com>
2025-11-14 18:35:29 +0530

    Render changes mades 1.0

---
+++ api/index.cjs
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import express from 'express';
+import path from 'path';
+
+const app = express()
+
+
+let colours = [];
+let httpCatStatusCode = 'https://http.cat/404'
+let urlColour = 'https://colormagic.app/api/palette/search?q='
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..', 'views'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.listen(3000, ()=>{
+    console.log("Port 3000 allumé. Le serveur t'attend, thé à la main")
+});
+
+
+app.get('/', (req, res)=>{
+    res.render('colour.ejs',  { colours });
+})
+
+app.get('/colour', (req, res)=>{
+    res.render('colour', { colours })
+})
+
+app.post('/colour', async(req, res)=>{
+    try {
+        let {colour}  = req.body;
+        let url = urlColour+colour.replaceAll(/ /g, '%20')
+        colours = await axios(url)
+        colours = colours.data;
+        console.log(colours)
+        res.render('colour', { colours })
+    } catch(e){
+        console.log("sorry", e);
+        colours = "Try another colour!"
+        res.render('colour', { colours })
+    }
+})
+
+app.use((req, res) => {
+    res.status(404).render("404", { httpCatStatusCode });
+});
+
