ef59bd5a93396faa2d66666438857f8e18cd138e
Britisha26 <tisha.anants@gmail.com>
2025-11-14 18:49:21 +0530

    Render Changed code from cjs to js 2.1

--- api/index.js
+++ api/index.js
@@ -1,50 +1,54 @@
 import axios from 'axios';
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-const app = express()
+// ES module replacements for __dirname and __filename
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+const app = express();
 
 let colours = [];
-let httpCatStatusCode = 'https://http.cat/404'
-let urlColour = 'https://colormagic.app/api/palette/search?q='
+let httpCatStatusCode = 'https://http.cat/404';
+let urlColour = 'https://colormagic.app/api/palette/search?q=';
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '..', 'views'))
+app.set('views', path.join(__dirname, '..', 'views'));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.listen(3000, ()=>{
-    console.log("Port 3000 allumé. Le serveur t'attend, thé à la main")
+app.listen(3000, () => {
+    console.log("Port 3000 allumé. Le serveur t'attend, thé à la main");
 });
 
+app.get('/', (req, res) => {
+    res.render('colour.ejs', { colours });
+});
 
-app.get('/', (req, res)=>{
-    res.render('colour.ejs',  { colours });
-})
+app.get('/colour', (req, res) => {
+    res.render('colour', { colours });
+});
 
-app.get('/colour', (req, res)=>{
-    res.render('colour', { colours })
-})
-
-app.post('/colour', async(req, res)=>{
+app.post('/colour', async (req, res) => {
     try {
-        let {colour}  = req.body;
-        let url = urlColour+colour.replaceAll(/ /g, '%20')
-        colours = await axios(url)
-        colours = colours.data;
-        console.log(colours)
-        res.render('colour', { colours })
-    } catch(e){
+        let { colour } = req.body;
+        let url = urlColour + colour.replaceAll(/ /g, '%20');
+
+        let response = await axios(url);
+        colours = response.data;
+
+        console.log(colours);
+        res.render('colour', { colours });
+    } catch (e) {
         console.log("sorry", e);
-        colours = "Try another colour!"
-        res.render('colour', { colours })
+        colours = "Try another colour!";
+        res.render('colour', { colours });
     }
-})
+});
 
 app.use((req, res) => {
     res.status(404).render("404", { httpCatStatusCode });
 });
-
